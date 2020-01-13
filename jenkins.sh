c9b32de4f779292fd7cf68d141043ca60ddd77dc
Aleksei Badyaev <aleksei.badyaev@gmail.com>
2020-01-13 23:42:59 +0700

    Доработана сборка пакетов.

--- jenkins.sh
+++ jenkins.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
+# Jenkins build script.
 
-# Build distro images.
 mv setup.py setup.py.bak
 mv setup_deb.py setup.py
-pip install -e .[dev]
+
+pip install -e .
+VERSION=$(python setup.py --version)
+PACKAGE_NAME="conan"
+PACKAGE_FILE="${PACKAGE_NAME}_${VERSION}-1"
+
+# Build distro images.
+pip install stdeb3
 python setup.py --command-packages=stdeb3.command bdist_deb
 rm -f ./*.tar.gz
+pushd "deb_dist/${PACKAGE_NAME}-${VERSION}"
+PYBUILD_DISABLE=test dpkg-buildpackage -uc -us
+popd
+
 mv setup.py setup_deb.py
 mv setup.py.bak setup.py
+python setup.py bdist_wheel
 
 mkdir -p build/
-VERSION=$(python setup.py --version)
 echo "${VERSION}" > ./VERSION
-
 sed\
  -e "s/VERSION/${VERSION}/g"\
  -e "s%BRANCH%$(git rev-parse --abbrev-ref HEAD | sed -e 's%/%-%g')%g"\
--- setup_deb.py
+++ setup_deb.py
@@ -112,10 +112,10 @@ setup(
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': dev_requirements,
-        'test': dev_requirements,
-    },
+    # extras_require={
+    #     'dev': dev_requirements,
+    #     'test': dev_requirements,
+    # },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
